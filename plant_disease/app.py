2b0904da6014701b47f2146c7410238f3e7c45e9
dali-tej <109671502+dali-tej@users.noreply.github.com>
2025-08-22 22:15:56 +0100

    Add files via upload

---
+++ plant_disease/app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import os
+
+# Always get the current directory of app.py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Load model + encoder using absolute paths
+model = joblib.load(os.path.join(BASE_DIR, "plant_disease_model.pkl"))
+encoder = joblib.load(os.path.join(BASE_DIR, "encoder.pkl"))
+
+#encoder dict
+categories_dict = {
+    "Leaf_Color": {cat: i for i, cat in enumerate(encoder.categories_[0])},
+    "Spots": {cat: i for i, cat in enumerate(encoder.categories_[1])}
+}
+
+# Add background image with CSS
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://marketplace.canva.com/EAFnfGcKtiM/1/0/1600w/canva-green-illustrative-organic-plant-zoom-virtual-background-fpJFOduK3w4.jpg");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+#title of the app
+st.title("🌱 Plant Disease Classifier")
+
+#input widgets
+leaf_color = st.selectbox("Leaf Color", list(categories_dict["Leaf_Color"].keys()))
+spots = st.selectbox("Spots", list(categories_dict["Spots"].keys()))
+moisture = st.slider("Moisture", 20, 90, 50)
+temperature = st.slider("Temperature (°C)", 15, 35, 25)
+
+#predict button
+if st.button("Predict Disease"):
+    input_df = pd.DataFrame({
+        "Leaf_Color": [categories_dict["Leaf_Color"][leaf_color]],
+        "Spots": [categories_dict["Spots"][spots]],
+        "Moisture": [moisture],
+        "Temperature": [temperature]
+    })
+    # prediction
+    prediction = model.predict(input_df)[0]
+    # prediction dispay
+    st.success(f"Predicted Disease: {prediction}")
